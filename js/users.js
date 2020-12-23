769ad365e24c453eea0a7c0c9856686be260ec78
LazySenpai <roman.kovaliov@student.howest.be>
2020-12-23 13:36:45 +0100

    javascript dropdown aanpassingen

--- js/users.js
+++ js/users.js
@@ -156,47 +156,6 @@
             }
         });
     }
-     function sortTable(tableClass, n) {
-         var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-
-         table = document.getElementById(tableClass);
-         switching = true;
-         dir = "asc";
-         while (switching) {
-             switching = false;
-             rows = table.getElementsByTagName("TR");
-             for (i = 1; i < (rows.length - 1); i++) {
-                 shouldSwitch = false;
-                 x = rows[i].getElementsByTagName("TD")[n];
-                 y = rows[i + 1].getElementsByTagName("TD")[n];
-                 var cmpX = isNaN(parseInt(x.innerHTML)) ? x.innerHTML.toLowerCase() : parseInt(x.innerHTML);
-                 var cmpY = isNaN(parseInt(y.innerHTML)) ? y.innerHTML.toLowerCase() : parseInt(y.innerHTML);
-                 cmpX = (cmpX == '-') ? 0 : cmpX;
-                 cmpY = (cmpY == '-') ? 0 : cmpY;
-                 if (dir == "asc") {
-                     if (cmpX > cmpY) {
-                         shouldSwitch = true;
-                         break;
-                     }
-                 } else if (dir == "desc") {
-                     if (cmpX < cmpY) {
-                         shouldSwitch = true;
-                         break;
-                     }
-                 }
-             }
-             if (shouldSwitch) {
-                 rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                 switching = true;
-                 switchcount++;
-             } else {
-                 if (switchcount == 0 && dir == "asc") {
-                     dir = "desc";
-                     switching = true;
-                 }
-             }
-         }
-     }
      function toggleProfiles() {
         var x = document.getElementById("dropprofielen");
         if (x.style.display === "none") {
@@ -205,3 +164,51 @@
           x.style.display = "none";
         }
       }
+      function clickAble(buttonId,divId){
+            $("#"+divId+" a").click(function () {
+                $("#"+buttonId +" .selection").text($(this).text());
+            });
+      }
+          //Van internet
+     function sortTable(tableClass, n) {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+        table = document.getElementById(tableClass);
+        switching = true;
+        dir = "asc";
+        while (switching) {
+            switching = false;
+            rows = table.getElementsByTagName("TR");
+            for (i = 1; i < (rows.length - 1); i++) {
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                var cmpX = isNaN(parseInt(x.innerHTML)) ? x.innerHTML.toLowerCase() : parseInt(x.innerHTML);
+                var cmpY = isNaN(parseInt(y.innerHTML)) ? y.innerHTML.toLowerCase() : parseInt(y.innerHTML);
+                cmpX = (cmpX == '-') ? 0 : cmpX;
+                cmpY = (cmpY == '-') ? 0 : cmpY;
+                if (dir == "asc") {
+                    if (cmpX > cmpY) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (cmpX < cmpY) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchcount++;
+            } else {
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
+//
